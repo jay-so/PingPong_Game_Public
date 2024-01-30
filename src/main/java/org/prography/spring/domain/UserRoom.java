11a5727c3850f68ed1d3f6c0f61eef46ee57175a
jay-so <merry9504@naver.com>
2024-01-30 23:11:32 +0900

    feat: UserRoom 엔티티 작성

---
+++ src/main/java/org/prography/spring/domain/UserRoom.java
@@ -0,0 +1,32 @@
+package org.prography.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.prography.spring.domain.enums.TeamStatus;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room roomId;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @NotNull
+    @Column(length = 25)
+    private TeamStatus teamStatus;
+}
