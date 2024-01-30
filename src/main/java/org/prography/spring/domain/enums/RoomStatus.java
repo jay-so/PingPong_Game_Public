b140510c71b6f8b741b049a41af487e1c6ef4f8c
jay-so <merry9504@naver.com>
2024-01-30 23:38:34 +0900

    feat: 방 상태(RoomStatus) Converter 구현

--- src/main/java/org/prography/spring/domain/enums/RoomStatus.java
+++ src/main/java/org/prography/spring/domain/enums/RoomStatus.java
@@ -1,7 +1,15 @@
 package org.prography.spring.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Getter
 @RequiredArgsConstructor
@@ -11,4 +19,18 @@ public enum RoomStatus {
     PROGRESS("진행중"),
     FINISH("완료"),
     ;
+
+    private static final Map<String, RoomStatus> roomStatusMap = Collections.unmodifiableMap(Stream.of(values())
+            .collect(Collectors.toMap(RoomStatus::getDescription, Function.identity())));
+
+    @JsonValue
+    private final String description;
+
+    @JsonCreator
+    public static RoomStatus from(String description) {
+        if (roomStatusMap.containsKey(description)) {
+            return roomStatusMap.get(description);
+        }
+        throw new IllegalArgumentException("RoomStatus not found");
+    }
 }
