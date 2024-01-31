6bcedb9dfd247aa8fc2b2de73db66a002468474b
jay-so <merry9504@naver.com>
2024-01-31 23:25:54 +0900

    feat: 서버 상태 API(헬스 체크 API) 구현

---
+++ src/main/java/org/prography/spring/controller/ServerStatusController.java
@@ -0,0 +1,21 @@
+package org.prography.spring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.prography.spring.common.ApiResponse;
+import org.prography.spring.service.ServerStatusService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/health")
+public class ServerStatusController {
+
+    private final ServerStatusService serverStatusService;
+
+    @GetMapping
+    public ApiResponse serverStatusCheck() {
+        return serverStatusService.serverStatusCheck();
+    }
+}
