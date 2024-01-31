2e5607ba856607a55922e4f9462cca71da5de3d3
jay-so <merry9504@naver.com>
2024-01-31 18:32:05 +0900

    feat: ApiResponse 구현 및 ApiResponseCode Enum 추가

---
+++ src/main/java/org/prography/spring/common/ApiResponseCode.java
@@ -0,0 +1,18 @@
+package org.prography.spring.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ApiResponseCode implements ResponseCode {
+
+    SUCCESS(200, "API 요청이 성공했습니다."),
+    BAD_REQUEST(201, "불가능한 요청입니다."),
+    SEVER_ERROR(500, "에러가 발생했습니다."),
+    ;
+
+
+    private final Integer code;
+    private final String message;
+}
