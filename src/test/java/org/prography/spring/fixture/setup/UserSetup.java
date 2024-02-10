d39e192aa2feab3454fd13010edea82fb1841096
jay-so <merry9504@naver.com>
2024-02-11 03:09:12 +0900

    test: User, Room, UserRoom 테스트 SetUp 클래스 작성

---
+++ src/test/java/org/prography/spring/fixture/setup/UserSetup.java
@@ -0,0 +1,26 @@
+package org.prography.spring.fixture.setup;
+
+import org.prography.spring.domain.User;
+import org.prography.spring.fixture.domain.UserFixture;
+import org.prography.spring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserSetup {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User setUpUser(Long fakerId) {
+        User user = UserFixture.userBuild(fakerId);
+        return userRepository.save(user);
+    }
+
+    public List<User> setUpUsers(int count) {
+        List<User> users = UserFixture.usersBuild(count);
+        return userRepository.saveAll(users);
+    }
+}
