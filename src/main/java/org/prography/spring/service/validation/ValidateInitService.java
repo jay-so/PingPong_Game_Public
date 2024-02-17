35bdc6127deafdc8b3bbb695be269c57d33fe35d
jay-so <merry9504@naver.com>
2024-02-17 13:31:13 +0900

    refactor: 초기화 API 서비스단 예외 클래스 분리 및 테스트 코드 리팩토링

---
+++ src/main/java/org/prography/spring/service/validation/ValidateInitService.java
@@ -0,0 +1,17 @@
+package org.prography.spring.service.validation;
+
+import org.prography.spring.common.BussinessException;
+import org.prography.spring.dto.request.InitializationRequest;
+import org.springframework.stereotype.Service;
+
+import static org.prography.spring.common.ApiResponseCode.BAD_REQUEST;
+
+@Service
+public class ValidateInitService {
+
+    public void validateInitializationRequest(InitializationRequest initializationRequest) {
+        if(!initializationRequest.validateInitializationRequest()){
+            throw new BussinessException(BAD_REQUEST);
+        }
+    }
+}
